36f3e929bb4157b4118005b5add87a77f04a5bcb
vicky-Bhandari-17 <vicky1001770@gmail.com>
2026-01-10 11:42:07 +0530

    Add files via upload

---
+++ Assigment1.py
@@ -0,0 +1,29 @@
+"""x= "hello" 
+print(x)
+
+add two numbers
+a=10
+b=20
+print("sum is = ",(a+b))
+
+Even or Odd
+
+num = int(input("Enter a number: "))
+
+ Check if even or odd using modulo operator
+if (num % 2 == 0):
+    print(" Even ")
+         
+else:
+   print(" Odd")
+
+check leap year
+year = int(input("enter year :"))
+if year % 4 == 0:
+    print("leap year")
+else:
+   print("not a leap year")"""
+        
+   # print pi value
+   import math
+      print (math.pi)
