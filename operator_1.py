a00f2de2ce4feac235a20f4b6a7e90533ffec9e3
vicky-Bhandari-17 <vicky1001770@gmail.com>
2026-01-28 11:23:12 +0530

    Add files via upload

---
+++ operator_1.py
@@ -0,0 +1,41 @@
+a = 12
+print(a)
+
+x= 20
+print (x)
+
+a=5
+print (-5)
+
+p= 10
+q=5
+print(p>q)
+print(p==q)
+
+x= True
+y= False
+print (x and y)
+print (x or y)
+print (not y)
+
+is_pass
+print (is_pass)
+
+a=5
+b=3
+print (a & b)
+print (a / b)
+
+numbers = [1,2,3,4]
+print (2 in numbers)
+print (5 not in numbers)
+
+x=10
+y=10
+print (x is y)
+print (x is not y)
+
+a=10
+b=3
+print (a + b)
+print (a % b)
