c4080add3b7ec0ef4bb7ff631e6aa92e1e3479da
vicky-Bhandari-17 <vicky1001770@gmail.com>
2026-01-31 11:27:24 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,13 @@
+age=int (input("Enter Your Age:"))
+if age>=18:
+        print("Yes Yor Are Eligible For Vote")
+else :
+        print(" Sorry you are not Eligible For Vote ")
+
+marks = int (input ("enter your marks :"))
+if marks >=90:
+    print("you will get grade A")
+elif marks >=60:
+    print("you will get grade B")
+else :
+    print("better luck next time")
