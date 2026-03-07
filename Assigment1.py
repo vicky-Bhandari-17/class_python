b307dccbe5719490c0281500e490dde522cf2e82
vicky-Bhandari-17 <vicky1001770@gmail.com>
2026-03-07 11:03:03 +0530

    Refactor Assignment1.py with new functionality

--- Assigment1.py
+++ Assigment1.py
@@ -1,29 +1,57 @@
-"""x= "hello" 
-print(x)
+# 1. print message
+print("Assignment")
 
-add two numbers
-a=10
-b=20
-print("sum is = ",(a+b))
+# 2. Enter value is print its sum
+a = int(input("Enter value : "))
+b = int(input("Enter value : "))
+print("Sum is = ",(a+b))
 
-Even or Odd
-
-num = int(input("Enter a number: "))
-
- Check if even or odd using modulo operator
-if (num % 2 == 0):
-    print(" Even ")
-         
+# 3. Check given input is ODD OR EVEN
+c = int(input("Enter value : "))
+if(c % 2 != 0):
+    print("Given value is ODD")
 else:
-   print(" Odd")
+    print("Given value is EVEN")
 
-check leap year
-year = int(input("enter year :"))
-if year % 4 == 0:
-    print("leap year")
+# 4. check Leap year
+year = int(input("Enter a year: "))
+
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+    print(year, "is a Leap Year")
 else:
-   print("not a leap year")"""
-        
-   # print pi value
-   import math
-      print (math.pi)
+    print(year, "is not a Leap Year")
+
+# 5. print PI value 
+PI = 3.14
+print(PI, "its a PI value")
+
+# 6. Constant value
+import math
+PI = math.pi
+print("PI value is : ",PI)
+
+# 7. Square of numbers
+square = int(input("Enter value : "))
+print("It square is : ",(square*square))
+
+# 8. Area of circle
+r = int(input("Enter value : "))
+circle = 2 * PI * r
+print("Area of circle is : ",circle)
+
+# Check Data Type
+data = input("Enter something : ")
+print(type(data))
+
+# 10. Use math function && 11. Find power
+import math
+number = 12
+power = pow(number, 2)
+print("PI value is : ",power)
+
+# 12. Check Positive or Negative
+x = int(input("Enter number : ")) 
+if(x >= 0) :
+    print("Your number is Positive ")
+else :
+    print("Your number is Nagative")
