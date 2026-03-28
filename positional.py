a015e1b4090e8be0128d6ecd3686d7903af3c32f
vicky-Bhandari-17 <vicky1001770@gmail.com>
2026-03-28 11:33:25 +0530

    Add files via upload

---
+++ positional.py
@@ -0,0 +1,17 @@
+# basic positional arguments:
+def add (b,a):
+    print("a =",a)
+    print("b =",b)
+    return a+b
+
+a = 3
+b = 4
+add(a, b)
+       
+# student information:
+def student_info(name,roll,marks):
+    print("name:",name)
+    print("roll:",roll)
+    print("marks:",marks)
+    
+student_info("vicky",101,90)
