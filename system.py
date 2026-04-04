35a0adbdeff14dac0c4746ecd396842df0dcfef1
vicky-Bhandari-17 <vicky1001770@gmail.com>
2026-04-04 10:49:20 +0530

    Add examples of various data types in Python

--- system.py
+++ system.py
@@ -1,7 +1,47 @@
-import sys
-print (sys.argv)
+iprint("Hello world!")
+
+# Data Types in Python
+
+# string
+a = "vicky"
+print(a)
+
+# int
+b = 17
+print(b)
 
 # float
-# str
-# int
+c = 22.17
+print(c)
+
+# list
+d = ["a","b","c","d",]
+print(d)
+
+# tuple
+e = ("a","b","c","d")
+print(e)
+
+# frozenset
+h = ({"a","b","c","d"})
+print(h)
+
 # bool
+f = True
+print(f)
+
+# nonetype
+g = None
+print(g)
+
+# Bytes
+i = b"Hello"
+print(i)
+
+# Range
+j = range(5)
+print(j)
+
+# complex numbers
+k = 3j
+print(k)
